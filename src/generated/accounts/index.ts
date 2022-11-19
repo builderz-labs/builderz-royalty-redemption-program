d9010acf4111101f1268b5e67495ef18addcde07
KultureElectric <rubenusshd@gmail.com>
2022-11-19 11:33:09 +0100

    test updates and rm sdk

--- src/generated/accounts/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './NftState'
-
-import { NftState } from './NftState'
-
-export const accountProviders = { NftState }
--- tests/repay_royalties_contract.ts
+++ tests/repay_royalties_contract.ts
@@ -27,15 +27,20 @@ describe("repay_royalties_contract", () => {
   it("Is initialized!", async () => {
 
     // NFTs
-    const mintListRaw = readFileSync("./3AVVU8NE28yhpZwrLv615kEP28i8t4buLfocAhhR7qQ5_mint_accounts.json", { encoding: "utf-8" })
-    const mintList: Array<string> = JSON.parse(mintListRaw);
-    const mintListPubkeys = mintList.map((mint: string) => new PublicKey(mint));
+    // const mintListRaw = readFileSync("./3AVVU8NE28yhpZwrLv615kEP28i8t4buLfocAhhR7qQ5_mint_accounts.json", { encoding: "utf-8" })
+    // const mintList: Array<string> = JSON.parse(mintListRaw);
+    // const mintListPubkeys = mintList.map((mint: string) => new PublicKey(mint));
     
     const metaplex = new Metaplex(connection);
 
-    const nfts = await metaplex.nfts().findAllByMintList({ mints: mintListPubkeys });    
+    // const nfts = await metaplex.nfts().findAllByMintList({ mints: mintListPubkeys });    
 
-    const testNFT = nfts[2]
+    // const testNFT = nfts[2];
+
+    const testNFT = await metaplex.nfts().findByMint({ mintAddress: new PublicKey("J9MGK8Lns8qiFRLRPb2RhPQn5kfLWzbqXsdNaHTWw61K") })
+
+    console.log(testNFT);
+    
 
     const creators = testNFT.creators;
     const remainingAccounts = [];
@@ -51,7 +56,7 @@ describe("repay_royalties_contract", () => {
 
     const [nftStateAddress] = PublicKey.findProgramAddressSync(
       // @ts-ignore
-      [Buffer.from("nft-state"), testNFT.mintAddress.toBuffer()],
+      [Buffer.from("nft-state"), testNFT.mint.address.toBuffer()],
       PROGRAM_ID
     )
     
@@ -72,19 +77,19 @@ describe("repay_royalties_contract", () => {
     tx.add(createRepayRoyaltiesInstruction({
       nftState: nftStateAddress,
       // @ts-ignore
-      nftMint: testNFT.mintAddress,
-      nftMintMetadata: testNFT.address,
+      nftMint: testNFT.mint.address,
+      nftMintMetadata: testNFT.metadataAddress,
       user: kp.publicKey,
       anchorRemainingAccounts: remainingAccounts,
     }, {
-      latestSaleLamports: 1 * LAMPORTS_PER_SOL
+      latestSaleLamports: 0.001 * LAMPORTS_PER_SOL
     }))
 
     const blockhash = await connection.getLatestBlockhash();
 
     tx.feePayer = kp.publicKey;
     tx.recentBlockhash = blockhash.blockhash;
-    wallet.signTransaction(tx);
+    await wallet.signTransaction(tx);
 
     const sig = await connection.sendRawTransaction(tx.serialize(), { skipPreflight: true });
     console.log(sig);
@@ -96,7 +101,7 @@ describe("repay_royalties_contract", () => {
       lastValidBlockHeight: blockhash.lastValidBlockHeight
     });
 
-    console.log(confirmation);
+    console.log(confirmation.value.err.toString());
 
     nftStateAccount = await tryGetAccount();
 
